e48efd0ad7447891966022d99a76340adc38167c
George Treviranus <geowtrev@gmail.com>
2022-02-26 16:04:50 -0600

    refactor: use function for new lines in script

--- bin/helpers/reporter.js
+++ bin/helpers/reporter.js
@@ -5,7 +5,15 @@ import ora from "ora"
 export const reporter = ora()
 export const pkgReporter = ora({ indent: 4 })
 
+function newConsoleLine() {
+  /* eslint-disable-next-line no-console */
+  console.log("")
+}
+
 export function exitWithError(error, message) {
-  reporter.fail(`Error: ${message}`)
+  newConsoleLine()
+  /* eslint-disable-next-line no-console */
+  console.error("Error:", error)
+  reporter.fail(message)
   process.exit(1)
 }
